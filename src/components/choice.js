ead5fe0ae847d4cfa0340f35c1baee2850562e87
Ben Anderson <banderson@hubspot.com>
2018-06-20 01:56:44 -0400

    Spike of the "Choose a Message" page

---
+++ src/components/choice.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Flex, Box, Heading } from 'rebass';
+import Button from '@material-ui/core/Button';
+
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: 'rgb(236, 171, 164)',
+      dark: 'rgb(236, 171, 164)',
+      contrastText: "rgba(255, 255, 255, 0.87)"
+    },
+  },
+  typography: {
+    fontSize: 18,
+    button: {
+      textTransform: "none",
+    },
+  },
+});
+
+const Blurb = styled.p`
+  margin-top: 8px;
+`;
+
+export default ({ message, title }) => {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <Flex alignItems="center" mb={10}>
+        <Box width={4 / 6}>
+          <Heading fontSize={28} mt={3}>{title}</Heading>
+          <Blurb>{message}</Blurb>
+        </Box>
+        <Box pl={4} width={1 / 6}>
+          <Flex flexDirection="column">
+            <Button disableRipple variant="contained" color="primary" size="large">Copy</Button>
+          </Flex>
+        </Box>
+      </Flex>
+    </MuiThemeProvider>
+  );
+};
--- src/components/pages/choices.js
+++ src/components/pages/choices.js
@@ -4,27 +4,24 @@ import { Flex, Box } from 'rebass';
 import Button from '@material-ui/core/Button';
 
 import Page from '../page';
+import Choice from '../choice';
 
 const Header = styled.h1`
   font-family: peace_sansregular;
-  font-size: 64px;
+  font-size: 72px;
   font-weight: normal;
   color: rgb(236, 171, 164);
   text-transform: uppercase;
-  margin-bottom: 25px;
+  margin-bottom: 0;
 `;
 
-// const theme = createMuiTheme({
-//   palette: {
-//     primary: {
-//       main: 'rgb(236, 171, 164)',
-//       dark: 'rgb(236, 171, 164)',
-//     },
-//     text: {
-//       primary: 'rgb(236, 171, 164)',
-//     },
-//   },
-// });
+const SubHead = styled.p`
+  font-size: 22px;
+  font-weight: bolder;
+  line-height: 1.5;
+  margin-top: 0;
+  margin-bottom: 50px;
+`;
 
 export default class Form extends React.Component {
   state = {
@@ -34,35 +31,15 @@ export default class Form extends React.Component {
 
   render() {
     return (
-      <Page align="default">
-        <Header>Your OOO Messages</Header>
-        <p>
-          Pick the out of office message that will make your contacts smile.
-          Copy it to your clipboard and paste it into your email settings. Then
-          get out of here. It's vacation time!
-        </p>
-        <form>
-          <Flex justify="center">
-            <Box px={3} pt={4} width={1 / 3}>
-              <Button
-                style={{ width: '100%' }}
-                required
-                label="First name"
-                id="first"
-                name="first"
-              />
-            </Box>
-            <Box px={3} pt={4} width={1 / 3}>
-              <Button
-                style={{ width: '100%' }}
-                required
-                label="Last name"
-                id="last"
-                name="last"
-              />
-            </Box>
-          </Flex>
-        </form>
+      <Page align="default" maxWidth={1200}>
+        <Header>OOO Messages</Header>
+        <SubHead>
+          Pick the out of office message that will make your contacts smile.<br />
+          Copy it to your clipboard and paste it into your email settings.<br />
+          Then get out of here. It's vacation time!
+        </SubHead>
+        <Choice title="Message #1" message="Bits of moving fluff dream of the mind's eye kindling the energy hidden in matter. Drake Equation billions upon billions made in the interiors of collapsing stars courage of our questions stirred by starlight, rogue Hypatia not a sunrise but a galaxyrise. Realm of the galaxies, paroxysm of global death Euclid." />
+        <Choice title="Message #2" message="Cambrian explosion made in the interiors of collapsing stars light years, astonishment Drake Equation cosmic ocean, trillion culture, colonies, at the edge of forever. Hundreds of thousands laws of physics! Stirred by starlight. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit dispassionate extraterrestrial observer consciousness realm of the galaxies courage of our questions and billions upon billions upon billions upon billions upon billions upon billions upon billions." />
       </Page>
     );
   }
