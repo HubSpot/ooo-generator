f2a572ac8487ac373a3752aeeac8ac80f88694f3
Nicholas Hwang <hello@undefined.engineer>
2018-07-12 10:46:31 -0400

    add clipboard copy feature

--- src/components/choice.js
+++ src/components/choice.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Flex as _Flex, Box, Heading } from 'rebass';
+import { Flex, Box, Heading } from 'rebass';
 import Button from '@material-ui/core/Button';
+import Clipboard from 'clipboard';
 
 import { ChoiceStyles } from './style/themes';
 
@@ -10,39 +11,66 @@ const Blurb = styled.p`
   margin-top: 8px;
 `;
 
-const Flex = styled(_Flex)`
+const StyledFlex = styled(Flex)`
   @media (max-width: 768px) {
     flex-flow: column;
     align-items: center;
   }
 `;
 
-const Choice = ({ message, title }) => {
-  return (
-    <ChoiceStyles>
-      <Flex alignItems="center" mb={10}>
-        <Box width={4 / 5}>
-          <Heading fontSize={28} mt={3}>
-            {title}
-          </Heading>
-          <Blurb>{message}</Blurb>
-        </Box>
-        <Box pl={4} width={1 / 5}>
-          <Flex flexDirection="column">
-            <Button
-              disableRipple
-              variant="contained"
-              color="primary"
-              size="large"
-            >
-              Copy
-            </Button>
-          </Flex>
-        </Box>
-      </Flex>
-    </ChoiceStyles>
-  );
-};
+const withWatermark = message =>
+  `${message}\n\nMade with love by HubSpot's Out-Of-Office Email Generator`;
+
+class Choice extends Component {
+  constructor() {
+    super();
+    this.ref = React.createRef();
+    this.clipboard = null;
+  }
+
+  componentDidMount() {
+    const { message } = this.props;
+    const { current: $btn } = this.ref;
+
+    this.clipboard = new Clipboard($btn, {
+      text: () => withWatermark(message),
+    });
+  }
+
+  componentWillUnmount() {
+    this.clipboard.destroy();
+  }
+
+  render() {
+    const { message, title } = this.props;
+
+    return (
+      <ChoiceStyles>
+        <StyledFlex alignItems="center" mb={10}>
+          <Box width={4 / 5}>
+            <Heading fontSize={28} mt={3}>
+              {title}
+            </Heading>
+            <Blurb>{message}</Blurb>
+          </Box>
+          <Box pl={4} width={1 / 5}>
+            <Flex flexDirection="column">
+              <Button
+                buttonRef={$btn => (this.ref.current = $btn)}
+                color="primary"
+                disableRipple
+                size="large"
+                variant="contained"
+              >
+                Copy
+              </Button>
+            </Flex>
+          </Box>
+        </StyledFlex>
+      </ChoiceStyles>
+    );
+  }
+}
 
 Choice.propTypes = {
   message: PropTypes.string,
