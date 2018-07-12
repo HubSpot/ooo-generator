e996b166b4ca5e42045cf71de77f8fdbc6d4c023
Nicholas Hwang <hello@undefined.engineer>
2018-07-12 01:48:10 -0400

    Updates

--- path---index-a4c2183c0e8003bd2e3b.js
+++ path---index-a4c2183c0e8003bd2e3b.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x81b8806e4260],{518:function(t,e){t.exports={data:{site:{id:"Site",siteMetadata:{title:"Out of Office Email Generator",tagline:"Because taking vacation should be fun."}}},pathContext:{}}}});
+webpackJsonp([0x81b8806e4260],{612:function(t,e){t.exports={data:{site:{id:"Site",siteMetadata:{title:"Out of Office Email Generator",tagline:"Because taking vacation should be fun."}}},pathContext:{}}}});
 //# sourceMappingURL=path---index-a4c2183c0e8003bd2e3b.js.map
