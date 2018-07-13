c33eae8e699eb4f16180da641f6a3d78c762e346
Nicholas Hwang <hello@undefined.engineer>
2018-07-13 12:32:55 -0400

    just replace route back to index on 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,6 @@
-import React from 'react';
-
-const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-);
+const NotFoundPage = () => {
+  window.location.replace('/');
+  return null;
+};
 
 export default NotFoundPage;
