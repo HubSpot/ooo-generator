622877644f9a307f32fcd8ef1e4f04771b3e1f50
Nicholas Hwang <nick.joosung.hwang@gmail.com>
2018-07-12 20:44:32 -0400

    add clipboard copy to buttons (#15)
    * add clipboard.js
    
    * add clipboard copy feature
    
    * add simple tooltip sans styles

--- src/components/choice.js
+++ src/components/choice.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Flex as _Flex, Box, Heading } from 'rebass';
+import { Flex, Box, Heading } from 'rebass';
 import Button from '@material-ui/core/Button';
+import Tooltip from '@material-ui/core/Tooltip';
+import Clipboard from 'clipboard';
 
 import { ChoiceStyles } from './style/themes';
 
@@ -10,39 +12,87 @@ const Blurb = styled.p`
   margin-top: 8px;
 `;
 
-const Flex = styled(_Flex)`
+const StyledFlex = styled(Flex)`
   @media (max-width: 768px) {
     flex-flow: column;
     align-items: center;
   }
 `;
 
-const Choice = ({ message, title }) => {
-  return (
-    <ChoiceStyles>
-      <Flex alignItems="center" mb={10}>
-        <Box width={4 / 5}>
-          <Heading fontSize={28} mt={3}>
-            {title}
-          </Heading>
-          <Blurb>{message}</Blurb>
-        </Box>
-        <Box pl={4} width={1 / 5}>
-          <Flex flexDirection="column">
-            <Button
-              disableRipple
-              variant="contained"
-              color="primary"
-              size="large"
-            >
-              Copy
-            </Button>
-          </Flex>
-        </Box>
-      </Flex>
-    </ChoiceStyles>
-  );
-};
+const withWatermark = message =>
+  `${message}\n\nMade with love by HubSpot's Out-Of-Office Email Generator`;
+
+class Choice extends Component {
+  constructor() {
+    super();
+    this.ref = React.createRef();
+    this.clipboard = null;
+
+    this.state = {
+      timer: null,
+    };
+  }
+
+  componentDidMount() {
+    const { message } = this.props;
+    const { current: $btn } = this.ref;
+
+    this.clipboard = new Clipboard($btn, {
+      text: () => withWatermark(message),
+    });
+
+    this.clipboard.on('success', this.createTimer);
+  }
+
+  componentWillUnmount() {
+    this.clipboard.destroy();
+  }
+
+  createTimer = () => {
+    const { timer } = this.state;
+    clearTimeout(timer);
+    this.setState({
+      timer: setTimeout(() => {
+        this.setState({
+          timer: null,
+        });
+      }, 2500),
+    });
+  };
+
+  render() {
+    const { message, title } = this.props;
+    const { timer } = this.state;
+
+    return (
+      <ChoiceStyles>
+        <StyledFlex alignItems="center" mb={10}>
+          <Box width={4 / 5}>
+            <Heading fontSize={28} mt={3}>
+              {title}
+            </Heading>
+            <Blurb>{message}</Blurb>
+          </Box>
+          <Box pl={4} width={1 / 5}>
+            <Flex flexDirection="column">
+              <Tooltip open={timer != null} placement="top" title="Copied!">
+                <Button
+                  buttonRef={$btn => (this.ref.current = $btn)}
+                  color="primary"
+                  disableRipple
+                  size="large"
+                  variant="contained"
+                >
+                  Copy
+                </Button>
+              </Tooltip>
+            </Flex>
+          </Box>
+        </StyledFlex>
+      </ChoiceStyles>
+    );
+  }
+}
 
 Choice.propTypes = {
   message: PropTypes.string,
