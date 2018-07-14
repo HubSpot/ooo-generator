e422962c4f9c17894ff1b6bce45aa2fb52f4a79d
Nicholas Hwang <hello@undefined.engineer>
2018-07-13 22:08:31 -0400

    slightly reduce contrast

--- src/components/style/tokens.js
+++ src/components/style/tokens.js
@@ -1,4 +1,4 @@
-export const whiteish = 'rgb(255, 255, 255)';
+export const whiteish = 'rgb(250, 250, 250)';
 export const pinkish = 'rgb(239, 133, 172)';
 export const pinkishWithOpacity = 'rgba(239, 133, 172, 0.85)';
-export const blackish = 'rgb(68, 68, 68)';
+export const blackish = 'rgb(80, 80, 80)';
