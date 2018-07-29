f2cf80121f6dc29f8ef8dc33eccc2261c20304e5
Nicholas Hwang <hello@undefined.engineer>
2018-07-29 14:34:02 -0400

    use pointer on share buttons

--- src/components/sharing.js
+++ src/components/sharing.js
@@ -10,23 +10,29 @@ import {
 } from 'react-share';
 
 const URL = 'https://www.ooogenerator.com';
-const DESCRIPTION = "Vacation is exciting. So why isn't your OOO reply? Use this Out of Office Email Generator to get a personalized (and witty) reply to copy and paste in your inbox:";
+const DESCRIPTION =
+  "Vacation is exciting. So why isn't your OOO reply? Use this Out of Office Email Generator to get a personalized (and witty) reply to copy and paste in your inbox:";
 
 const Wrapper = styled.div`
   margin-bottom: 16px;
 `;
 
-const StyledFacebookShareButton = styled(FacebookShareButton)`
+const BaseShareButton = `
   display: inline-block;
+  cursor: pointer;
+`;
+
+const StyledFacebookShareButton = styled(FacebookShareButton)`
+  ${BaseShareButton};
   margin-right: 16px;
 `;
 
 const StyledTwitterShareButton = styled(TwitterShareButton)`
-  display: inline-block;
+  ${BaseShareButton};
 `;
 
 const StyledLinkedinShareButton = styled(LinkedinShareButton)`
-  display: inline-block;
+  ${BaseShareButton};
   margin-left: 16px;
 `;
 
@@ -38,11 +44,7 @@ const Sharing = () => (
     >
       <FacebookIcon round={true} size={36} />
     </StyledFacebookShareButton>
-    <StyledTwitterShareButton
-      url={URL}
-      title={DESCRIPTION}
-      via="HubSpot"
-    >
+    <StyledTwitterShareButton url={URL} title={DESCRIPTION} via="HubSpot">
       <TwitterIcon round={true} size={36} />
     </StyledTwitterShareButton>
     <StyledLinkedinShareButton
