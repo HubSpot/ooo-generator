ef36844c4c469870e8ca596230eb4c6e7d7528ff
Nicholas Hwang <hello@undefined.engineer>
2018-12-10 11:23:18 -0500

    fix use of rgba

--- src/components/style/tokens.js
+++ src/components/style/tokens.js
@@ -1,4 +1,4 @@
 export const white = 'rgb(250, 250, 250)';
 export const gray = 'rgb(234, 240, 246)';
-export const black = 'rgb(51, 71, 91, 1.0)';
-export const slate = 'rgb(51, 71, 91, 0.75)';
+export const black = 'rgba(51, 71, 91, 1.0)';
+export const slate = 'rgba(51, 71, 91, 0.75)';
