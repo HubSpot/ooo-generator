378c1977c3a382cf9c8bcd9735f4ca19ef50cd92
aem <amarkon895@gmail.com>
2020-09-17 15:09:10 +0000

    Deploying to gh-pages from  @ 36e6f6ebf3420ea1897a70eaa1faf06265a942e2 🚀

---
+++ component---src-pages-404-js-ccef021d3a5aa162e37e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(n,t,o){"use strict";o.r(t);var e=o("9Hrx"),r=o("q1tI"),i=function(n){function t(){return n.apply(this,arguments)||this}Object(e.a)(t,n);var o=t.prototype;return o.componentDidMount=function(){window.location.replace("/")},o.render=function(){return null},t}(r.Component);t.default=i}}]);
+//# sourceMappingURL=component---src-pages-404-js-ccef021d3a5aa162e37e.js.map
